baa5fef91e31f1d718474ccd834e612716faa75f
Thys Jamneck <thys@unomena.com>
2018-01-26 12:10:46 +0200

    Removed patterns

--- registration/auth_urls.py
+++ registration/auth_urls.py
@@ -10,11 +10,11 @@ under:
 * User logout at ``logout/``.
 
 * The two-step password change at ``password/change/`` and
-  ``password/change/done/``.
+    ``password/change/done/``.
 
 * The four-step password reset at ``password/reset/``,
-  ``password/reset/confirm/``, ``password/reset/complete/`` and
-  ``password/reset/done/``.
+    ``password/reset/confirm/``, ``password/reset/complete/`` and
+    ``password/reset/done/``.
 
 The default registration backend already has an ``include()`` for
 these URLs, so under the default setup it is not necessary to manually
@@ -28,31 +28,53 @@ from django.conf.urls import *
 from django.contrib.auth import views as auth_views
 
 
-urlpatterns = patterns('',
-                       url(r'^login/$',
-                           auth_views.login,
-                           {'template_name': 'registration/login.html'},
-                           name='auth_login'),
-                       url(r'^logout/$',
-                           auth_views.logout,
-                           {'template_name': 'registration/logout.html'},
-                           name='auth_logout'),
-                       url(r'^password/change/$',
-                           auth_views.password_change,
-                           name='auth_password_change'),
-                       url(r'^password/change/done/$',
-                           auth_views.password_change_done,
-                           name='auth_password_change_done'),
-                       url(r'^password/reset/$',
-                           auth_views.password_reset,
-                           name='auth_password_reset'),
-                       url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
-                           auth_views.password_reset_confirm,
-                           name='auth_password_reset_confirm'),
-                       url(r'^password/reset/complete/$',
-                           auth_views.password_reset_complete,
-                           name='auth_password_reset_complete'),
-                       url(r'^password/reset/done/$',
-                           auth_views.password_reset_done,
-                           name='auth_password_reset_done'),
-)
+urlpatterns = [
+    url(
+        r'^login/$',
+        auth_views.login,
+        {
+            'template_name': 'registration/login.html'
+        },
+        name='auth_login'
+    ),
+
+    url(
+        r'^logout/$',
+        auth_views.logout,
+        {
+            'template_name': 'registration/logout.html'
+        },
+        name='auth_logout'
+    ),
+
+    url(
+        r'^password/change/$',
+        auth_views.password_change,
+        name='auth_password_change'
+    ),
+    url(
+        r'^password/change/done/$',
+        auth_views.password_change_done,
+        name='auth_password_change_done'
+    ),
+    url(
+        r'^password/reset/$',
+        auth_views.password_reset,
+        name='auth_password_reset'
+    ),
+    url(
+        r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
+        auth_views.password_reset_confirm,
+        name='auth_password_reset_confirm'
+    ),
+    url(
+        r'^password/reset/complete/$',
+        auth_views.password_reset_complete,
+        name='auth_password_reset_complete'
+    ),
+    url(
+        r'^password/reset/done/$',
+        auth_views.password_reset_done,
+        name='auth_password_reset_done'
+    ),
+]
--- registration/backends/default/urls.py
+++ registration/backends/default/urls.py
@@ -6,7 +6,7 @@ If the default behavior of these views is acceptable to you, simply
 use a line like this in your root URLconf to set up the default URLs
 for registration::
 
-    (r'^accounts/', include('registration.backends.default.urls')),
+        (r'^accounts/', include('registration.backends.default.urls')),
 
 This will also automatically set up the views in
 ``django.contrib.auth`` at sensible default locations.
@@ -25,27 +25,43 @@ from registration.views import activate
 from registration.views import register
 
 
-urlpatterns = patterns('',
-                       url(r'^activate/complete/$',
-                           generic_views.TemplateView.as_view(template_name='registration/activation_complete.html'),
-                           name='registration_activation_complete'),
-                       # Activation keys get matched by \w+ instead of the more specific
-                       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
-                       # that way it can return a sensible "invalid key" message instead of a
-                       # confusing 404.
-                       url(r'^activate/(?P<activation_key>\w+)/$',
-                           activate,
-                           {'backend': 'registration.backends.default.DefaultBackend'},
-                           name='registration_activate'),
-                       url(r'^register/$',
-                           register,
-                           {'backend': 'registration.backends.default.DefaultBackend'},
-                           name='registration_register'),
-                       url(r'^register/complete/$',
-                           generic_views.TemplateView.as_view(template_name='registration/registration_complete.html'),
-                           name='registration_complete'),
-                       url(r'^register/closed/$',
-                           generic_views.TemplateView.as_view(template_name='registration/registration_closed.html'),
-                           name='registration_disallowed'),
-                       (r'', include('registration.auth_urls')),
-                       )
+urlpatterns = patterns(
+    url(
+        r'^activate/complete/$',
+        generic_views.TemplateView.as_view(
+            template_name='registration/activation_complete.html'
+        ),
+        name='registration_activation_complete'
+    ),
+     # Activation keys get matched by \w+ instead of the more specific
+     # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+     # that way it can return a sensible "invalid key" message instead of a
+     # confusing 404.
+    url(
+        r'^activate/(?P<activation_key>\w+)/$',
+        activate,
+        {'backend': 'registration.backends.default.DefaultBackend'},
+        name='registration_activate'
+    ),
+    url(
+        r'^register/$',
+        register,
+        {'backend': 'registration.backends.default.DefaultBackend'},
+        name='registration_register'
+    ),
+    url(
+        r'^register/complete/$',
+        generic_views.TemplateView.as_view(
+            template_name='registration/registration_complete.html'
+        ),
+        name='registration_complete'
+    ),
+    url(
+        r'^register/closed/$',
+        generic_views.TemplateView.as_view(
+            template_name='registration/registration_closed.html'
+        ),
+        name='registration_disallowed'
+    ),
+    url(r'', include('registration.auth_urls')),
+)
