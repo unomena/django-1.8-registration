ee83c9746463752f933671b1fac076e872235a15
Thys Jamneck <thys@unomena.com>
2018-01-26 12:17:18 +0200

    upped the version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-1.8-registration',
-    version='0.1.0',
+    version='0.1.1',
     description='Django Registration patched for Django 1.8',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Unomena',
